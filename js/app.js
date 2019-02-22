532c213fe2814b90d91a37e87889a7f4a9a5b4e7
Redaloukil <loukil.red@gmail.com>
2019-02-22 23:53:01 +0100

    test best distance function , initiate grasp research function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,45 @@
-var numberOfCities = 9;
-var cities = [];
+const numberOfCities = 8;
 
-function hello(){
+const cities = [];
+
+var bestPath = 0;
+
+var city = {
+        lar  :0,
+        long :0,
+}
+
+
+function chooseToStart(){
+    
+}
+
+function grasp(){
 
 }
+
+function swap(a , i , j){
+        var temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+}
+
+//ELECTION OF RESTRICTED CANDIDATES LIST
+function bestPathFinder(bPath , path){
+    if (path < bPath){
+        return path;
+    }
+    return bPath
+}
+
+function calculateDistance(cities){
+        var distance = 0;
+        for (var i = 0 ;i < cities.length -1; i++){
+            distance = distance + dist(cities[i].x , cities[i].y , cities[i+1].x , cities[i+1].y ) 
+        } 
+        return distance
+}
+
 
 function setup(){
     createCanvas(600 , 600)
@@ -13,14 +49,38 @@ function setup(){
     }
 }
 
-
 function draw(){
     
+    frameRate(2)
     background(0);
-    
     fill(255);
     
     for (var i=0; i<cities.length; i++){
-        ellipse(cities[i].x , cities[i].y , 12 , 12);
+        ellipse(cities[i].x , cities[i].y , 6 , 6);
     }
+
+    stroke(255);
+    strokeWeight(2);
+    
+    beginShape();
+    noFill();
+    
+    for (var i=0; i<cities.length; i++){
+        vertex(cities[i].x , cities[i].y );
+    }
+    
+    endShape();
+    
+    bestPath = bestPathFinder(bestPath , calculateDistance(cities));
+    console.log(bestPath);
+    var i = floor(random(cities.length));
+    var j = floor(random(cities.length));
+
+    swap(cities , i , j)
 }
+
+
+
+
+
+
